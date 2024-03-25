c31a705e20cf1be4fbe90d4cf5c5888f2b381f02
guhzoide <hugorobertiferreira61.br@gmail.com>
2024-03-25 20:53:14 -0300

    Finalizando Django: autenticação de formulários e alerta

--- inserir_banco.py
+++
@@ -1,6 +0,0 @@
-from galeria.models import Fotografia
-
-foto = Fotografia(nome="Pilares da criação", legenda="nasa.org / NASA / James Webb", foto="pilares-da-criacao.webp") 
-foto.save()
-
-Fotografia.objects.all()
