b6821ea8e9f5649490a252d8b9f9ff2736108eb6
Hugo Ferreira <hugorobertiferreira61.br@gmail.com>
2024-04-09 20:05:38 -0300

    Avançando com curso de dajngo na Alura

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from apps.galeria.models import Fotografia
 from django.contrib import messages
+from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
+from django.shortcuts import render, get_object_or_404, redirect
 
 def index(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
         return redirect('login')
 
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
@@ -27,3 +28,24 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
+    form = FotografiaForms
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Imagem adicionada com sucesso')
+            return redirect('index')
+
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
